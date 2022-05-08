af34bacffc02811e8aa5568ebc95b5faae364d67
v-xuzhigang <jxdxmx@126.com>
2022-05-08 08:41:09 +0800

    2022-5-8

--- interview/012.大量协程同时申请内存GC-Pool.go
+++ interview/012.大量协程同时申请内存GC-Pool.go
@@ -20,6 +20,12 @@ func F012() {
 			processRequest(1 << 28) // 256MiB
 		}
 	}()
+
+	//原因：size为1<<28的goroutine大概率会Get到1<<10的bytes.Buffer，
+	//然后再grow，也就是把1k的那个缓存grow到了256M。那自然了，慢慢慢慢的，
+	//大量10kbuffer被grow成了256M，内存就暴涨了。
+	//由于goroutine太多，所以没法GC，仍然会残留大量对象在pool中。因此会OOM。
+	//如果把【processRequest(1 << 10) // 1KiB】去掉，就不会暴涨。这是因为，每次都能获取到缓存的对象，不存在从1k>>256M的情形。
 	for i := 0; i < 20; i++ {
 		go func() { // 这个地方注释掉,就不会OOM了
 			for {
